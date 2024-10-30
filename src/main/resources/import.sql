171467e227f5244e7ae3b027ffd93db2de8b2d89
guischossler <g521g99@gmail.com>
2024-10-30 14:17:30 -0300

    ajuste no insert do import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,9 +3,3 @@ INSERT INTO tb_persons (name, email) VALUES ('Mariana Costa', 'mariana.costa@exa
 INSERT INTO tb_persons (name, email) VALUES ('João Pereira', 'joao.pereira@example.com');
 INSERT INTO tb_persons (name, email) VALUES ('Ana Souza', 'ana.souza@example.com');
 INSERT INTO tb_persons (name, email) VALUES ('Paulo Oliveira', 'paulo.oliveira@example.com');
-
-INSERT INTO tb_books (name, author) VALUES ('The Great Gatsby', 'F. Scott Fitzgerald');
-INSERT INTO tb_books (name, author) VALUES ('To Kill a Mockingbird', 'Harper Lee');
-INSERT INTO tb_books (name, author) VALUES ('1984', 'George Orwell');
-INSERT INTO tb_books (name, author) VALUES ('Pride and Prejudice', 'Jane Austen');
-INSERT INTO tb_books (name, author) VALUES ('Moby-Dick', 'Herman Melville');
