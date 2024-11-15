150545fab1a8cf8e3784901f3c91b96e8fa77a06
guischossler <g521g99@gmail.com>
2024-11-14 22:39:02 -0300

    removidas varias linhas desnecessarias e duplicadas

--- src/main/java/com/meuteste/Meu/Teste/controllers/RentalController.java
+++ src/main/java/com/meuteste/Meu/Teste/controllers/RentalController.java
@@ -1,9 +1,6 @@
 package com.meuteste.Meu.Teste.controllers;
 
 import com.meuteste.Meu.Teste.entities.Rental;
-import com.meuteste.Meu.Teste.entities.Book;
-import com.meuteste.Meu.Teste.entities.Person;
-import com.meuteste.Meu.Teste.repositories.BookRepository;
 import com.meuteste.Meu.Teste.repositories.RentalRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,11 +31,6 @@ public class RentalController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Rental> findById(@PathVariable Long id) {
         Optional<Rental> rental = repository.findById(id);
-        if (rental.isPresent()) {
-            return ResponseEntity.ok(rental.get());
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-
         if (!rental.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
@@ -91,37 +83,6 @@ public class RentalController {
 
         repository.deleteById(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-    }
-
-    @PutMapping(value = "/{id}")
-    public ResponseEntity<Rental> update(@PathVariable Long id, @RequestBody @Valid Rental rental) {
-        if (!repository.existsById(id)) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-
-        rental.setId(id);
-        Rental updatedRental = repository.save(rental);
-        return ResponseEntity.ok(updatedRental);
-    }
-
-    @PatchMapping(value = "/{id}")
-    public ResponseEntity<Rental> partialUpdate(@PathVariable Long id, @RequestBody Rental rental) {
-        Optional<Rental> existingRentalOpt = repository.findById(id);
-        if (existingRentalOpt.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-
-        Rental existingRental = existingRentalOpt.get();
-
-        if (rental.getBook() != null && rental.getBook().getId() != null) {
-            existingRental.setBook(rental.getBook());
-        }
-        if (rental.getPerson() != null && rental.getPerson().getId() != null) {
-            existingRental.setPerson(rental.getPerson());
-        }
-
-        repository.save(existingRental);
-        return ResponseEntity.ok(existingRental);
     }
 
 }
